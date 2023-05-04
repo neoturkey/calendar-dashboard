0cbd15379c5d6f3b0306fe651c12d1076a20ebf5
James Loxam <jamesloxam@gmail.com>
2023-05-04 13:24:47 +0100

    Lots of "first time" set-up including linting and gapi initial set-up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
+import React from 'react';
+
+import { useGapi, withGapi } from './components/GAPI';
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const test = useGapi();
+    console.log('Test', test);
+
+    // const calendarID = 'family11306879740916668579@group.calendar.google.com';
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload. 2
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
-export default App;
+export default withGapi(App, {
+    apiKey: process.env.REACT_APP_GOOGLE_CALENDAR_API_KEY,
+    clientId: process.env.REACT_APP_GOOGLE_CALENDAR_CLIENT_ID,
+});
---
+++ src/components/GAPI.js
@@ -0,0 +1,78 @@
+import React from 'react';
+
+import { gapi } from 'gapi-script';
+
+const DISCOVERY_DOC =
+    'https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest';
+const SCOPES = 'https://www.googleapis.com/auth/calendar.readonly';
+
+const GapiContext = React.createContext();
+
+const GapiProvider = (props) => {
+    const [gapiClient, setGapiClient] = React.useState();
+    const [gapiClientInitialised, setGapiClientInitialised] =
+        React.useState(false);
+    const [gapiClientSignedIn, setGapiClientSignedIn] = React.useState();
+
+    React.useEffect(() => {
+        const run = async () => {
+            await gapi.load('client', function () {
+                gapi.client
+                    .init({
+                        apiKey: props.apiKey,
+                        clientId: props.clientId,
+                        discoveryDocs: [DISCOVERY_DOC],
+                        scope: SCOPES,
+                    })
+                    .then(async function () {
+                        setGapiClientInitialised(true);
+                        setGapiClient(gapi);
+
+                        const gapiAuth = gapi.auth2.getAuthInstance();
+
+                        gapiAuth.isSignedIn.listen(setGapiClientSignedIn);
+                        setGapiClientSignedIn(gapiAuth.isSignedIn.get());
+                        // if (signedIn) {
+                        //     const resp = await gapi.client.request({
+                        //         // path: `https://www.googleapis.com/calendar/v3/calendars/${calendarID}/events`,
+                        //         path: 'https://www.googleapis.com/calendar/v3/users/me/calendarList',
+                        //     });
+                        //     console.log('Resp', resp);
+                        // }
+                    });
+            });
+        };
+        run()
+            .then(() => {
+                console.log('Done');
+            })
+            .catch((err) => {
+                console.error('GAPI Error', err);
+            });
+    }, []);
+
+    return (
+        <GapiContext.Provider
+            value={{ gapiClient, gapiClientInitialised, gapiClientSignedIn }}
+        >
+            {props.children}
+        </GapiContext.Provider>
+    );
+};
+
+export const withGapi = (InnerComponent, withOpts = {}) => {
+    const GapiHOC = (props) => (
+        <GapiProvider {...withOpts}>
+            <InnerComponent {...props} />
+        </GapiProvider>
+    );
+    return GapiHOC;
+};
+
+export const useGapi = () => {
+    const context = React.useContext(GapiContext);
+    if (context === undefined || context === null) {
+        throw new Error('useUserContext must be called within UserProvider');
+    }
+    return context;
+};
