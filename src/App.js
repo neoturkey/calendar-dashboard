c809e6c6040e693f0b8c9aeab1c9b6aa94d58d89
James Loxam <jamesloxam@gmail.com>
2023-05-15 17:52:31 +0100

    Refactor DashboardView to separate file

--- src/App.js
+++ src/App.js
@@ -1,53 +1,15 @@
 import React from 'react';
 
 import { useGapi, withGapi } from './components/GAPI';
-import { useDataManager, withDataManager } from './components/DataManager';
+import { withDataManager } from './components/DataManager';
 import { withTimeManager } from './components/TimeManager';
-import EventList from './components/EventList';
+
 import WelcomeScreen from './components/WelcomeScreen';
 
-import dayjs from 'dayjs';
 import _ from 'lodash';
-import { Box } from '@mui/material';
-import ClockView from './components/ClockView';
-
-function groupEventsForTwoDayOutlook(events) {
-    const today = {
-        name: 'Today',
-        events: [],
-    };
-    const tomorrow = {
-        name: 'Tomorrow',
-        events: [],
-    };
-
-    const endOfToday = dayjs().endOf('day');
-
-    _.each(events, (e) => {
-        if (endOfToday.isBefore(e.startTimestamp)) tomorrow.events.push(e);
-        else today.events.push(e);
-    });
-
-    return [today, tomorrow];
-}
-
-function eventsForWeekend(events) {
-    const weekendStart = dayjs().add(-1, 'day').startOf('week').add(6, 'day');
-    const weekendEnd = weekendStart.endOf('day').add(1, 'day');
-
-    return (
-        events &&
-        _.filter(events, (event) => {
-            return (
-                event.startTimestamp.isBefore(weekendEnd) &&
-                weekendStart.isBefore(event.endTimestamp)
-            );
-        })
-    );
-}
+import DashboardView from './components/DashboardView';
 
 function App() {
-    const { events, eventsForGroup } = useDataManager();
     const { gapiClientSignedIn } = useGapi();
 
     if (gapiClientSignedIn === undefined) {
@@ -60,96 +22,7 @@ function App() {
         return <WelcomeScreen />;
     }
 
-    const twoDayLimit = dayjs().startOf('day').add(2, 'day');
-
-    return (
-        <Box
-            sx={{
-                width: '100%',
-                height: '100vh',
-                padding: 2,
-                boxSizing: 'border-box',
-
-                // backgroundColor: 'purple',
-                display: 'grid',
-                gridTemplateColumns: '1fr '.repeat(6),
-                gridTemplateRows: '1fr '.repeat(6),
-                gap: 2,
-            }}
-        >
-            <EventList
-                sx={{
-                    gridRow: '1 / 4',
-                }}
-                eventGroups={groupEventsForTwoDayOutlook(
-                    eventsForGroup('Erin', { maxTimestamp: twoDayLimit })
-                )}
-                title="Erin"
-                colorScheme="#ef6aef"
-            />
-            <EventList
-                sx={{
-                    gridRow: '1 / 4',
-                }}
-                eventGroups={groupEventsForTwoDayOutlook(
-                    eventsForGroup('Max', { maxTimestamp: twoDayLimit })
-                )}
-                title="Max"
-            />
-            <EventList
-                sx={{
-                    gridRow: '1 / 4',
-                }}
-                eventGroups={groupEventsForTwoDayOutlook(
-                    eventsForGroup('Gus', { maxTimestamp: twoDayLimit })
-                )}
-                title="Gus"
-            />
-            <EventList
-                sx={{
-                    gridRow: '1 / 4',
-                }}
-                eventGroups={groupEventsForTwoDayOutlook(
-                    eventsForGroup('Imi', { maxTimestamp: twoDayLimit })
-                )}
-                title="Imi"
-            />
-            <EventList
-                sx={{
-                    gridRow: '1 / 4',
-                }}
-                eventGroups={groupEventsForTwoDayOutlook(
-                    eventsForGroup('James', { maxTimestamp: twoDayLimit })
-                )}
-                title="James"
-            />
-            <EventList
-                sx={{
-                    gridRow: '1 / 4',
-                }}
-                title="Family"
-                eventGroups={groupEventsForTwoDayOutlook(
-                    eventsForGroup(undefined, { maxTimestamp: twoDayLimit })
-                )}
-            />
-            <EventList
-                sx={{
-                    gridColumn: '1 / 3',
-                    gridRow: '4 / 7',
-                }}
-                title="This Weekend"
-                events={eventsForWeekend(events)}
-                showAvatars={true}
-                showDay={true}
-            />
-            <ClockView
-                sx={{
-                    gridColumn: '3 / 7',
-                    gridRow: '6',
-                }}
-            />
-        </Box>
-    );
+    return <DashboardView />;
 }
 
 export default _.flowRight([
---
+++ src/components/DashboardView.js
@@ -0,0 +1,137 @@
+import EventList from './EventList';
+import { Box } from '@mui/material';
+import ClockView from './ClockView';
+import { useDataManager } from './DataManager';
+
+import dayjs from 'dayjs';
+import _ from 'lodash';
+
+function groupEventsForTwoDayOutlook(events) {
+    const today = {
+        name: 'Today',
+        events: [],
+    };
+    const tomorrow = {
+        name: 'Tomorrow',
+        events: [],
+    };
+
+    const endOfToday = dayjs().endOf('day');
+
+    _.each(events, (e) => {
+        if (endOfToday.isBefore(e.startTimestamp)) tomorrow.events.push(e);
+        else today.events.push(e);
+    });
+
+    return [today, tomorrow];
+}
+
+function eventsForWeekend(events) {
+    const weekendStart = dayjs().add(-1, 'day').startOf('week').add(6, 'day');
+    const weekendEnd = weekendStart.endOf('day').add(1, 'day');
+
+    return (
+        events &&
+        _.filter(events, (event) => {
+            return (
+                event.startTimestamp.isBefore(weekendEnd) &&
+                weekendStart.isBefore(event.endTimestamp)
+            );
+        })
+    );
+}
+
+export default function DashboardView() {
+    const { events, eventsForGroup } = useDataManager();
+
+    const twoDayLimit = dayjs().startOf('day').add(2, 'day');
+
+    return (
+        <Box
+            sx={{
+                width: '100%',
+                height: '100vh',
+                padding: 2,
+                boxSizing: 'border-box',
+
+                // backgroundColor: 'purple',
+                display: 'grid',
+                gridTemplateColumns: '1fr '.repeat(6),
+                gridTemplateRows: '1fr '.repeat(6),
+                gap: 2,
+            }}
+        >
+            <EventList
+                sx={{
+                    gridRow: '1 / 4',
+                }}
+                eventGroups={groupEventsForTwoDayOutlook(
+                    eventsForGroup('Erin', { maxTimestamp: twoDayLimit })
+                )}
+                title="Erin"
+                colorScheme="#ef6aef"
+            />
+            <EventList
+                sx={{
+                    gridRow: '1 / 4',
+                }}
+                eventGroups={groupEventsForTwoDayOutlook(
+                    eventsForGroup('Max', { maxTimestamp: twoDayLimit })
+                )}
+                title="Max"
+            />
+            <EventList
+                sx={{
+                    gridRow: '1 / 4',
+                }}
+                eventGroups={groupEventsForTwoDayOutlook(
+                    eventsForGroup('Gus', { maxTimestamp: twoDayLimit })
+                )}
+                title="Gus"
+            />
+            <EventList
+                sx={{
+                    gridRow: '1 / 4',
+                }}
+                eventGroups={groupEventsForTwoDayOutlook(
+                    eventsForGroup('Imi', { maxTimestamp: twoDayLimit })
+                )}
+                title="Imi"
+            />
+            <EventList
+                sx={{
+                    gridRow: '1 / 4',
+                }}
+                eventGroups={groupEventsForTwoDayOutlook(
+                    eventsForGroup('James', { maxTimestamp: twoDayLimit })
+                )}
+                title="James"
+            />
+            <EventList
+                sx={{
+                    gridRow: '1 / 4',
+                }}
+                title="Family"
+                eventGroups={groupEventsForTwoDayOutlook(
+                    eventsForGroup(undefined, { maxTimestamp: twoDayLimit })
+                )}
+            />
+            <EventList
+                sx={{
+                    gridColumn: '1 / 3',
+                    gridRow: '4 / 7',
+                }}
+                title="This Weekend"
+                events={eventsForWeekend(events)}
+                showAvatars={true}
+                showDay={true}
+            />
+            <ClockView
+                sx={{
+                    gridColumn: '3 / 7',
+                    gridRow: '6',
+                }}
+            />
+        </Box>
+    );
+}
